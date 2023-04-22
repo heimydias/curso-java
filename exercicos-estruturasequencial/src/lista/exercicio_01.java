261550b9334b0e0c8246381e8d887b243ed0110f
Heimy Dias <heimysantana@hotmail.com>
2023-04-21 22:12:36 -0300

    atualizações

---
+++ exercicos-estruturasequencial/src/lista/exercicio_01.java
@@ -0,0 +1,28 @@
+package lista;
+import java.util.Scanner;
+
+public class exercicio_01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a; 
+		int b;
+		int soma;
+		
+		System.out.print("Digite o 1º número: ");
+		a = sc.nextInt();
+		System.out.print("Digite o 2º número: ");
+		b = sc.nextInt();
+		
+		soma = a+b;
+		
+		System.out.println("Soma = " + soma);
+		
+		sc.close();
+		
+	}
+	
+	
+}
---
+++ exercicos-estruturasequencial/src/lista/exercico_03.java
@@ -0,0 +1,33 @@
+package lista;
+import java.util.Scanner;
+
+public class exercico_03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int valor1; 
+		int valor2;
+		int valor3;
+		int valor4;
+		int diferenca;
+		
+		System.out.print("Digite o 1º número: ");
+		valor1 = sc.nextInt();
+		System.out.print("Digite o 2º número: ");
+		valor2 = sc.nextInt();
+		System.out.print("Digite o 3º número: ");
+		valor3 = sc.nextInt();
+		System.out.print("Digite o 4º número: ");
+		valor4 = sc.nextInt();
+		
+		diferenca = (valor1 * valor2) - (valor3 * valor4);
+		
+		System.out.println("Diferença = " + diferenca);
+		
+		sc.close();
+
+	}
+	
+}
