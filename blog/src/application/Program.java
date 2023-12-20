4c5bfb2acb28304ce9df0b0431b862cac88c8298
Bowie <heimysantana@hotmail.com>
2023-12-19 23:13:12 -0300

    exercico posts

---
+++ blog/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import entities.Comment;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's wesome!");
+        Post p1 = new Post(
+                sdf.parse("18/12/2023 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        Comment c3 = new Comment("Good night");
+        Comment c4 = new Comment("May the force be with you");
+        Post p2 = new Post(
+                sdf.parse("13/12/2023 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                4);
+        p2.addComment(c3);
+        p2.addComment(c4);
+
+        System.out.println(p1);commit -,m
+        System.out.println(p2);
+    }
+}
